06a0e83c9087e628ccb43d48a4b3e09c38e961ca
br0k3ngl255 <alex@vaiolabs.com>
2019-01-01 21:01:10 +0200

    init adding the files

---
+++ 0_getting_started/hello.lua
@@ -0,0 +1,5 @@
+#!/usr/bin/env lua
+
+print "hello world!"
+var="hello world from var"
+print(var)
---
+++ 1_basics/guess_the_number.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/env  lua
+
+print("guess the number")
+math.randomseed(os.time())
+
+math.random()
+num = math.random(100)
+
+print("random num is :",num)
